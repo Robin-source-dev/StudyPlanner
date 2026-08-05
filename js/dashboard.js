6e2486399f185d0c840633a7ea3ece44c015cdbf
Robin <robbinmwenda2@gmail.com>
2026-08-05 11:35:10 +0300

    Add function to display upcoming tasks

--- js/dashboard.js
+++ js/dashboard.js
@@ -29,3 +29,64 @@ function loadDashboard(){
     //Display upcoming tasks
     displayUpcomingTasks(tasks)
 }
+
+//Displays upcoming tasks
+
+function displayUpcomingTasks(tasks){
+    upcomingTasks.innerHTML = ""
+    if(tasks.length === 0){
+        upcomingTasks.innerHTML = `
+            <p class="empty-message">
+                No study tasks available.
+                Click <strong>Add Task</strong> to get started.
+            </p>
+        `
+        return
+    }
+
+    //Sort by nearest due date
+    tasks.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
+
+    //Show only pending tasks
+    const pendingTasksOnly = tasks.filter(task => !task.completed)
+
+    //Display first five
+    const upcoming = pendingTasksOnly.slice(0, 5)
+
+    if (upcoming.length === 0) {
+
+        upcomingTasks.innerHTML = `
+            <p class="empty-message">
+                🎉 Congratulations!
+                You have completed all your study tasks.
+            </p>
+        `
+
+        return
+
+    }
+
+    upcoming.forEach(task => {
+
+        const card = document.createElement("div")
+
+        card.className = "task-card"
+
+        card.innerHTML = `
+
+            <h3>${task.title}</h3>
+
+            <p><strong>Course:</strong> ${task.course}</p>
+
+            <p><strong>Due:</strong> ${formatDate(task.dueDate)}</p>
+
+            <p><strong>Priority:</strong> ${task.priority}</p>
+
+            <p>${task.description}</p>
+
+        `
+
+        upcomingTasks.appendChild(card)
+
+    })
+}
